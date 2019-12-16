ea73704c7f4a6e83f7e1325dc84a0faf887c3d2d
Mad <github@typ03.de>
2019-12-16 12:16:18 +0100

    disable injuries/health fix by default

--- Source/LittleThings/Settings.cs
+++ Source/LittleThings/Settings.cs
@@ -7,7 +7,7 @@ namespace LittleThings
         public bool MoraleFixes = true;
         public bool UIFixes = true;
         public bool DisableTutorials = true;
-        public bool InjuryFixes = true;
+        public bool InjuryFixes = false;
         public bool AdjustMechPartCost = true;
         public double AdjustMechPartCostMultiplier = 0.8;
 
