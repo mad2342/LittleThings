f721a92a8f591bc5c65a28b049a261a836799437
Mad <github@typ03.de>
2019-12-16 10:23:40 +0100

    add option to disable tutorial popups

---
+++ Source/LittleThings/Patches/TutorialPopups.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using BattleTech;
+using BattleTech.UI;
+using Harmony;
+
+namespace LittleThings.Patches
+{
+    class TutorialPopups
+    {
+        // Suppress tutorial slides for Raven and Three Years Later
+        [HarmonyPatch(typeof(CombatHUD), "OnTriggerDialog")]
+        public static class CombatHUD_OnTriggerDialog_Patch
+        {
+            public static bool Prepare()
+            {
+                return LittleThings.Settings.DisableTutorials;
+            }
+
+            public static bool Prefix(CombatHUD __instance, MessageCenterMessage message)
+            {
+                try
+                {
+                    TriggerDialog triggerDialog = message as TriggerDialog;
+
+                    if (triggerDialog == null)
+                    {
+                        return false;
+                    }
+
+                    if (
+                        triggerDialog.DialogID == __instance.Combat.Constants.RavenTutorialDialogID ||
+                        triggerDialog.DialogID == __instance.Combat.Constants.Story2TutorialSlides1ID ||
+                        triggerDialog.DialogID == __instance.Combat.Constants.Story2TutorialSlides2ID ||
+                        triggerDialog.DialogID == __instance.Combat.Constants.Story2TutorialSlides3ID ||
+                        triggerDialog.DialogID == __instance.Combat.Constants.Story2TutorialSlides4ID 
+                        )
+                    {
+                        Logger.LogLine("[CombatHUD_OnTriggerDialog_PREFIX] Supress tutorial: "+ triggerDialog.DialogID);
+                        return false;
+                    }
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Logger.LogError(e);
+                    return true;
+                }
+            }
+        }
+    }
+}
--- Source/LittleThings/Settings.cs
+++ Source/LittleThings/Settings.cs
@@ -6,6 +6,7 @@ namespace LittleThings
     {
         public bool MoraleFixes = true;
         public bool UIFixes = true;
+        public bool DisableTutorials = true;
         public bool InjuryFixes = true;
         public bool AdjustMechPartCost = true;
         public double AdjustMechPartCostMultiplier = 0.8;
