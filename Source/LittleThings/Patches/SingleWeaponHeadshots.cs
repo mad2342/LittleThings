b1b2f112f348a0c8a97ee44c1d68501507a225d0
Mad <github@typ03.de>
2020-01-10 23:13:47 +0100

    add beta feature headshot weapon limit

--- Source/LittleThings/Patches/SingleWeaponHeadshots.cs
+++ Source/LittleThings/Patches/SingleWeaponHeadshots.cs
@@ -9,6 +9,7 @@ namespace LittleThings.Patches
 {
     class SingleWeaponHeadshots
     {
+        // NOTE: This setter is only called for Called Shots. See CombatHUDAttackModeSelector.Update() for the surrounding if-clause...
         [HarmonyPatch(typeof(CombatHUDAttackModeSelector), "DisplayedLocation", MethodType.Setter)]
         public static class CombatHUDAttackModeSelector_DisplayedLocation_Patch
         {
@@ -35,33 +36,59 @@ namespace LittleThings.Patches
                 {
                     //Logger.LogLine("[CombatHUDAttackModeSelector_DisplayedLocation_POSTFIX] Called.");
 
+                    CombatHUD ___HUD = (CombatHUD)AccessTools.Property(typeof(CombatHUDAttackModeSelector), "HUD").GetValue(__instance, null);
+
+                    // Prepping Called Shot
                     if (___displayedLocation == ArmorLocation.None)
                     {
-                        CombatHUD ___HUD = (CombatHUD)AccessTools.Property(typeof(CombatHUDAttackModeSelector), "HUD").GetValue(__instance, null);
+                        // Safeguarding even more...
                         if (
                             ___HUD.SelectionHandler.ActiveState.SelectionType != SelectionType.FireMorale ||
-                            ___HUD.CalledShotPopUp.ShownAttackDirection == AttackDirection.FromBack ||
-                            ___HUD.SelectedTarget.IsShutDown ||
-                            ___HUD.SelectedTarget.IsProne
+                            ___HUD.SelectedTarget.UnitType != UnitType.Mech
                         )
                         {
                             return;
                         }
 
+                        if (___HUD.SelectedTarget.IsShutDown || ___HUD.SelectedTarget.IsProne)
+                        {
+                            Color c = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.whiteHalf;
+                            __instance.DescriptionText.SetText("{0}{1}IMMOBILIZED{2}", new object[]
+                            {
+                                "^^^ SELECT A LOCATION FOR CALLED SHOT ^^^ \n",
+                                "<color=#" + ColorUtility.ToHtmlStringRGBA(c) + "><size=80%>",
+                                "</size></color>"
+                            });
+                            return;
+                        }
+
+                        if (___HUD.CalledShotPopUp.ShownAttackDirection == AttackDirection.FromBack)
+                        {
+                            Color c = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.whiteHalf;
+                            __instance.DescriptionText.SetText("{0}{1}REAR ARC{2}", new object[]
+                            {
+                                "^^^ SELECT A LOCATION FOR CALLED SHOT ^^^ \n",
+                                "<color=#" + ColorUtility.ToHtmlStringRGBA(c) + "><size=80%>",
+                                "</size></color>"
+                            });
+                            return;
+                        }
+
+                        // Regular Precision Strike from front or sides
                         Pilot selectedPilot = ___HUD.SelectedActor.GetPilot();
                         int enabledWeaponCount = Utilities.GetReadiedWeaponCount(___HUD.SelectedActor.Weapons, ___HUD.SelectedTarget);
                         int maxAllowedWeapons = Utilities.GetMaxAllowedWeaponCountForHeadshots(selectedPilot);
                         int weaponsToDisable = Math.Max(0, enabledWeaponCount - maxAllowedWeapons);
-                        //string pluralizeSuffix = maxAllowedWeapons > 1 ? "s" : "";
+                        //string pluralizeSuffix = maxAllowedWeapons > 1 ? "S" : "";
                         string pluralizeSuffix = weaponsToDisable > 1 ? "S" : "";
 
                         //Logger.LogLine("[CombatHUDAttackModeSelector_DisplayedLocation_POSTFIX] Appending headshot note...");
                         if (weaponsToDisable >= 1)
                         {
                             Color c = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.whiteHalf;
-                            __instance.DescriptionText.SetText("{0}{1}- DISABLE {2} MORE WEAPON{3} FOR A HEADSHOT -{4}", new object[]
+                            __instance.DescriptionText.SetText("{0}{1}DISABLE {2} MORE WEAPON{3} FOR A HEADSHOT{4}", new object[]
                             {
-                                "^^^ SELECT A LOCATION FOR CALLED SHOT! ^^^ \n",
+                                "^^^ SELECT A LOCATION FOR CALLED SHOT ^^^ \n",
                                 "<color=#" + ColorUtility.ToHtmlStringRGBA(c) + "><size=80%>",
                                 weaponsToDisable,
                                 pluralizeSuffix,
@@ -71,13 +98,28 @@ namespace LittleThings.Patches
                         else
                         {
                             Color c = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.gold;
-                            __instance.DescriptionText.SetText("{0}{1}- HEADSHOT ENABLED! -{2}", new object[]
+                            __instance.DescriptionText.SetText("{0}{1}HEADSHOT ENABLED{2}", new object[]
                             {
-                                "^^^ SELECT A LOCATION FOR CALLED SHOT! ^^^ \n",
+                                "^^^ SELECT A LOCATION FOR CALLED SHOT ^^^ \n",
                                 "<color=#" + ColorUtility.ToHtmlStringRGBA(c) + "><size=80%>",
                                 "</size></color>"
                             });
                         }
+                    }
+                    // Called Shot set at location
+                    else
+                    {
+                        //string targetName = ___HUD.SelectedActor.DisplayName;
+                        //string readiedWeaponList = Utilities.GetReadiedWeaponsString(___HUD.SelectedActor.Weapons, ___HUD.SelectedTarget);
+                        string locationHealth = Utilities.GetLocationHealthString(___HUD.SelectedTarget, ___displayedLocation);
+
+                        __instance.DescriptionText.SetText("CALLED SHOT: {0}\n{1}{2}{3}", new object[]
+                        {
+                            Mech.GetLongArmorLocation(___displayedLocation),
+                            "<size=80%>",
+                            locationHealth,
+                            "</size>"
+                        });
                     }
                 }
                 catch (Exception e)
@@ -88,7 +130,7 @@ namespace LittleThings.Patches
         }
 
         /*
-        // Only to fix small UI problems with the decription text box resizing...
+        // Fix small UI problems with the decription text box resizing...
         [HarmonyPatch(typeof(SelectionStateFire), "FireButtonString", MethodType.Getter)]
         public static class SelectionStateFire_FireButtonString_Patch
         {
@@ -107,8 +149,8 @@ namespace LittleThings.Patches
                     }
                     if (__instance.SelectionType == SelectionType.FireMorale)
                     {
-                        Logger.LogLine("[SelectionStateFire_FireButtonString_POSTFIX] Appending headshot note...");
-                        __result = $" ^^^ SELECT A LOCATION FOR CALLED SHOT! ^^^ \n<size=80%>---</size>";
+                        Logger.LogLine("[SelectionStateFire_FireButtonString_POSTFIX] Appending placeholder note...");
+                        __result = $" ^^^ SELECT A LOCATION FOR CALLED SHOT ^^^ \n<size=80%> </size>";
                     }   
                 }
                 catch (Exception e)
--- Source/LittleThings/Utilities.cs
+++ Source/LittleThings/Utilities.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BattleTech;
+using BattleTech.UI;
+using HBS;
+using UnityEngine;
 
 namespace LittleThings
 {
@@ -29,6 +33,74 @@ namespace LittleThings
             }
             return readiedWeaponCount;
         }
+
+
+
+        public static string GetLocationHealthString(ICombatant combatant, ArmorLocation location)
+        {
+            string result = "";
+
+            if (combatant.UnitType != UnitType.Mech)
+            {
+                return "NO MECH";
+            }
+
+            Mech m = combatant as Mech;
+
+            Color g = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.gold;
+            Color w = LazySingletonBehavior<UIManager>.Instance.UIColorRefs.whiteHalf;
+            string colorTagG = $"<color=#{ColorUtility.ToHtmlStringRGBA(g)}>";
+            string colorTagW = $"<color=#{ColorUtility.ToHtmlStringRGBA(w)}>";
+            string closeColorTag = "</color>";
+
+            int currentArmor = (int)m.GetCurrentArmor(location);
+            int maxArmor = (int)HUDMechArmorReadout.GetInitialArmorForLocation(m.MechDef, location);
+            int currentStructure = Math.Max(1, ((int)m.GetCurrentStructure(MechStructureRules.GetChassisLocationFromArmorLocation(location))));
+            int maxStructure = (int)m.GetMaxStructure(MechStructureRules.GetChassisLocationFromArmorLocation(location));
+            int totalCurrent = currentArmor + currentStructure;
+            int totalMax = maxArmor + maxStructure;
+
+            // Simple
+            if (currentStructure < maxStructure || currentArmor <= 0)
+            {
+                result = $"{colorTagG}LOCATION HEALTH: {totalCurrent} / {totalMax}{closeColorTag}";
+            }
+            else
+            {
+                result = $"{colorTagW}LOCATION HEALTH: {totalCurrent} / {totalMax}{closeColorTag}";
+            }
+
+            // Differentiated
+            //result = $"{colorTagG}STRUCTURE: {currentStructure} / {maxStructure}{closeColorTag}      {colorTagW}ARMOR: {currentArmor} / {maxArmor}{closeColorTag}";
+
+            return result;
+        }
+
+
+
+        public static string GetReadiedWeaponsString(List<Weapon> weapons, ICombatant target)
+        {
+            string result = "";
+            foreach (Weapon w in weapons)
+            {
+                bool willFireAtTarget = w.WillFireAtTarget(target);
+                if (w.IsEnabled && willFireAtTarget)
+                {
+                    result += $" | {w.Name}";
+                }
+            }
+            if(string.IsNullOrEmpty(result))
+            {
+                result = "-NO WEAPONS READY-";
+            }
+            else
+            {
+                result = result.Substring(3);
+            }
+            return result;
+        }
+
+
 
         public static int GetMaxAllowedWeaponCountForHeadshots(Pilot p, bool log = false)
         {
