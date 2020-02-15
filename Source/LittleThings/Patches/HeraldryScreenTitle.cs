d4baa872600a8bff5f3c409e34df377e54033cda
Mad <github@typ03.de>
2020-02-15 18:27:37 +0100

    add ui fix for heraldry screen title positioning

---
+++ Source/LittleThings/Patches/HeraldryScreenTitle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BattleTech.UI;
+using BattleTech.UI.TMProWrapper;
+using Harmony;
+using TMPro;
+using UnityEngine;
+
+namespace LittleThings.Patches
+{
+    class HeraldryScreenTitle
+    {
+        [HarmonyPatch(typeof(HeraldryCreatorPanel), "SetData")]
+        public static class HeraldryCreatorPanel_SetData_Patch
+        {
+            public static bool Prepare()
+            {
+                return LittleThings.Settings.UIFixes;
+            }
+
+            public static void Prefix(HeraldryCreatorPanel __instance, LocalizableText ___screenTitle, LocalizableText ___mechInfoName)
+            {
+                try
+                {
+                    Logger.Debug("[HeraldryCreatorPanel_SetData_PREFIX] Fix position of screen title");
+
+                    TMP_FontAsset ___mechInfoName_m_baseFont = (TMP_FontAsset)AccessTools.Field(typeof(LocalizableText), "m_baseFont").GetValue(___mechInfoName);
+
+                    ___screenTitle.fontSize = 44;
+                    ___screenTitle.SetFont(___mechInfoName_m_baseFont);
+                    //___screenTitle.text = "Heraldry Customization";
+
+                    RectTransform rt = ___screenTitle.GetComponent<RectTransform>();
+                    Vector3 pos = rt.localPosition;
+
+                    pos.x = -23;
+                    pos.y = 30;
+                    rt.localPosition = pos;
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
+            }
+        }
+    }
+}
